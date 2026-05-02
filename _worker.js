2384240eb081cd39200026cc6a57f20a568b1d26
梦未 <159781497+wytx0710@users.noreply.github.com>
2026-05-02 15:36:19 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -22,7 +22,7 @@ export default {
     if (!object) {
       try {
         // ====================== 在这里填你的源站 ======================
-        const originUrl = "https://ros.liuyue.net" + url.pathname;
+        const originUrl = "https://majo.cn-nb1.rains3.com" + url.pathname;
         // ==============================================================
 
         const originResponse = await fetch(originUrl);
